4bfcf4660d5c6a7dd56a98a7a6dfbba47f9fad5d
chenjiahong <chenjiahong@datasega.com>
2024-02-03 16:55:30 +0800

    初始化

---
+++ src/components/XtxHotBrand.vue
@@ -0,0 +1,56 @@
+<template>
+    <!-- 热门品牌 -->
+    <div class="hot">
+        <div class="wrapper">
+        <div class="title">
+            <div class="left">
+            <h3>热门品牌</h3>
+            <p>国际经典 品质认证</p>
+            </div>
+            <div class="button">
+            <a href="#"><i class="iconfont icon-arrow-left-bold"></i></a>
+            <a href="#"><i class="iconfont icon-arrow-right-bold"></i></a>
+            </div>
+        </div>
+        <div class="bd">
+            <ul>
+            <li>
+                <a href="#">
+                <img src="@/assets/images/hot1.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <img src="@/assets/images/hot2.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <img src="@/assets/images/hot3.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <img src="@/assets/images/hot4.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <img src="@/assets/images/hot5.png" alt="" />
+                </a>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
